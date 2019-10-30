25d37095c5d6365732885218d3b4baa3b8f47f0f
Fabio Barbieri <fabio.barbieri@vallesistemas.com.br>
2019-10-30 08:35:51 -0300

    Ex2

---
+++ src/main/java/aulas/manha/Ex2.java
@@ -0,0 +1,47 @@
+package aulas.manha;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+        /**
+         * Palindromo.
+         *
+         * Um palíndromo é uma palavra, frase que tenha a propriedade de poder
+         * ser lida tanto da direita para a esquerda como da esquerda para a
+         * direita. Num palíndromo, normalmente são desconsiderados os sinais
+         * ortográficos (diacríticos ou de pontuação), assim como o espaços
+         * entre palavras.
+         */
+        /**
+         * 1. Criar uma função que determine se um String é palindromo.
+         */
+        String palavra = ""; // Palavra a ser testada
+        System.out.println(palavra + (palimdromo(palavra) ? " é " : " não é ") + " palindromo.");
+
+        /**
+         * 2. Crie uma função que remova números duplicados de um Array ou List
+         */
+        
+        /**
+         * 3. Crie uma função que inverta as palavras em uma frase.
+         * 
+         * Ex: Entrada - "O Kaique está na sala"
+         *       Saida - "sala na está Kaique O"
+         * 
+         */
+        
+        /**
+         * 4. Crie uma função que inverta os algarismos em um número
+         *      
+         * Ex: Entrada - 123456
+         *       Saida - 654321
+         */
+        
+        
+    }
+
+    private static boolean palimdromo(String texto) {
+        throw new UnsupportedOperationException("Exercicio 1");
+    }
+
+}
