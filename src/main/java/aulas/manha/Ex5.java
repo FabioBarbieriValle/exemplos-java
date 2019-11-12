2177423105ef3906cdd05e81bcb48bd2fd71bd80
Fabio Barbieri <fabio.barbieri@vallesistemas.com.br>
2019-11-12 15:45:30 -0300

    Ex5

---
+++ src/main/java/aulas/manha/Ex5.java
@@ -0,0 +1,18 @@
+package aulas.manha;
+
+public class Ex5 {
+
+    public static void main(String[] args) {
+
+        /**
+         * 1. Criar uma função que mostre no console todas as combinacoes de
+         * letras de uma palavra (Não pode mostrar combinações repetidas).
+         *
+         * Exemplo: Entrada - 'GOD'
+         *          Saida - 'GOD','GDO','DGO','OGD','DOG','ODG'
+         *
+         */
+        
+        
+    }
+}
